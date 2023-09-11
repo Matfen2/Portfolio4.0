952221e62627d5c971f81b5cb32b2907eb085206
EyesEagle9 <110094556+EyesEagle9@users.noreply.github.com>
2023-09-11 14:04:40 +0200

    new commit

--- portfolio/src/app/main-page/show-projet/show-projet.component.ts
+++ portfolio/src/app/main-page/show-projet/show-projet.component.ts
@@ -1,28 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+interface ProjetDetail {
+  id: number;
+  name: string;
+  mainPict: string;
+  langageOne: string;
+  langageTwo: string;
+  langageThree: string;
+  hrefGithub: string;
+  hrefSite: string;
+}
+
 @Component({
   selector: 'app-show-projet',
   templateUrl: './show-projet.component.html',
-  styleUrls: ['./show-projet.component.css']
+  styleUrls: ['./show-projet.component.css'],
 })
-export class ShowProjetComponent implements OnInit  {
+export class ShowProjetComponent implements OnInit {
   projetId!: number;
-  projetData: any;
+  projetData: ProjetDetail | undefined; // Utilisez le type ProjetDetail ici
 
-  projetDetails = [
+  projetDetails: ProjetDetail[] = [
     {
       id: 1,
+      name: 'STARFIELD',
       mainPict: '/assets/pict/projets/starfieldSite.png',
       langageOne: 'ANGULAR',
       langageTwo: 'TYPESCRIPT',
       langageThree: 'SCSS',
-      hrefGithub: '',
-      hrefSite: '',
+      hrefGithub: 'https://github.com/Matfen2/Starfield',
+      hrefSite: 'https://starfield-first-db9775bbe509.herokuapp.com/main-page',
     },
     {
       id: 2,
-      mainPict: '',
+      name: 'BETHESDA GAME STUDIO',
+      mainPict: '/assets/pict/projets/bethesdaSite.png',
       langageOne: 'ANGULAR',
       langageTwo: 'TYPESCRIPT',
       langageThree: 'SCSS',
@@ -31,21 +44,24 @@ export class ShowProjetComponent implements OnInit  {
     },
     {
       id: 3,
-      mainPict: '',
+      name: 'ANIMUS',
+      mainPict: '/assets/pict/projets/animusSite.png',
       langageOne: 'ANGULAR',
       langageTwo: 'TYPESCRIPT',
       langageThree: 'SCSS',
-      hrefGithub: '',
-      hrefSite: '',
-    }
-  ]
+      hrefGithub: 'https://github.com/Matfen2/Animus',
+      hrefSite: 'https://animus-87d3c39892ed.herokuapp.com/main-page',
+    },
+  ];
 
-  constructor (private routes: ActivatedRoute) {}
+  constructor(private routes: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.routes.params.subscribe((params) => {
-      this.projetId =+ params['id'];
-      this.projetData = this.projetDetails.find((projet) => projet.id === this.projetId);
-    })
+      this.projetId = +params['id'];
+      this.projetData = this.projetDetails.find(
+        (projet) => projet.id === this.projetId
+      );
+    });
   }
 }
